5ea5b4057984feeea9a3f345566d737d64954558
Jeongeun Lee <jungeun9005@gmail.com>
2023-07-17 16:40:06 -0700

    add title

---
+++ js/carousel.js
@@ -0,0 +1,23 @@
+var nextName = document.getElementById("next-btn");
+var slideContainter = document.getElementsByClassName("slide-container")[0];
+
+var present = 1;
+nextName.addEventListener("click", function(){
+    if(present ==1) {
+        slideContainter.style.transform="translateX(-20vw)";
+        present += 1;
+    } else if (present == 2){
+        slideContainter.style.transform="translateX(-40vw)";
+    }
+});
+
+var preName = document.getElementById("pre-btn");
+
+preName.addEventListener("click",function(){
+    if(present ==2) {
+        slideContainter.style.transform="translateX(-20vw)";
+        present -= 1;  
+    } else if(present ==1) {
+        slideContainter.style.transform="translateX(0vw)"; 
+    }
+})
