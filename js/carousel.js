6a033933f5afa5a8dc9c35ea9fa659d5ecc7cec7
Jeongeun Lee <jungeun9005@gmail.com>
2023-07-19 16:28:15 -0700

    change the size

--- js/carousel.js
+++ js/carousel.js
@@ -4,10 +4,10 @@ var slideContainter = document.getElementsByClassName("slide-container")[0];
 var present = 1;
 nextName.addEventListener("click", function(){
     if(present ==1) {
-        slideContainter.style.transform="translateX(-20vw)";
+        slideContainter.style.transform="translateX(-40vw)";
         present += 1;
     } else if (present == 2){
-        slideContainter.style.transform="translateX(-40vw)";
+        slideContainter.style.transform="translateX(-80vw)";
     }
 });
 
@@ -15,7 +15,7 @@ var preName = document.getElementById("pre-btn");
 
 preName.addEventListener("click",function(){
     if(present ==2) {
-        slideContainter.style.transform="translateX(-20vw)";
+        slideContainter.style.transform="translateX(-80vw)";
         present -= 1;  
     } else if(present ==1) {
         slideContainter.style.transform="translateX(0vw)"; 
